58386ff5ebfb2b57f1341c8c752ad8eae617d628
Mauricio Covarrubias <mauricioecovarrubias@hotmail.com>
2021-02-20 09:55:27 -0600

    Add the algorithm code

--- indes.js
+++ indes.js
@@ -1,6 +1,21 @@
 function isValidSubsequence(array, sequence) {
   // Write your code here.
+  let j=0
+  for(i=0; i<array.length; i++)
+  {
+    if(sequence[j]===array[i])
+    {
+      j++
+    }
+  }
+  if(j===sequence.length)
+  {
+    console.log(true)
+  }
+  else
+  {
+    console.log(false)
+  }
 }
-
 // Do not edit the line below.
 exports.isValidSubsequence = isValidSubsequence;
